2450d9e578e8eccb49fcd481d1c0090197db00a6
Joel Wang <joelwangusa@gmail.com>
2024-04-09 18:50:33 -0400

    Sidebar with mobile view

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import { Menu } from "lucide-react"
+import { Menu, Sparkles } from "lucide-react"
 import Link from "next/link";
 import { Poppins } from "next/font/google";
 import { UserButton } from "@clerk/nextjs";
+import MobileSidebar from "@/components/mobilesidebar";
 
 import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
 
 const font = Poppins({
     weight: "600",
@@ -16,9 +19,9 @@ export const Navbar = () => {
     return (
       <div className="fixed w-full z-50 flex justify-between
       items-center py-2 px-2 border-b border-primary/10
-      bg-secondary">
+      bg-secondary h-16">
         <div className="flex items-center">
-          <Menu className="block md:hidden" />
+          <MobileSidebar  />
           <Link href="/">
               <h1 className={cn(
                 "hidden md:block text-xl md:text-3xl font-bold text-primary",
@@ -29,6 +32,11 @@ export const Navbar = () => {
           </Link>     
         </div>    
         <div className="flex items-center gap-x-3">
+          <Button variant="premium" size="sm">
+            Upgrade
+            <Sparkles className="w-4 h-4 fill-white text-white ml-2" />
+          </Button>
+          <ModeToggle />
           <UserButton />
         </div>         
       </div>
